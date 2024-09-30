4418574686bbf06dae940490432f011e16859641
Akash Thakur <akash0218.nitt@gmail.com>
2024-09-30 19:23:48 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env) => {
+  const htmlTemplate = "./src/index.html";
+  const plugins = [new HtmlWebpackPlugin({ template: htmlTemplate })];
+
+  const mode = env && env.prod ? "production" : "development";
+
+  return {
+    devtool: "inline-source-map",
+    entry: {
+      app: "./src/app.js",
+    },
+    mode,
+    output: {
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins,
+    devServer: {
+      open: true,
+      static: "./dist",
+    },
+  };
+};
